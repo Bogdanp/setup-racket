f9aca18de81343db5737c814f60ef632d10175db
Bogdan Popa <bogdan@defn.io>
2019-11-09 14:49:41 +0200

    core: add windows support

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
 import * as fs from 'fs';
@@ -6,6 +7,11 @@ export type Arch = 'x86' | 'x64';
 export type Variant = 'regular' | 'CS';
 export type Distribution = 'minimal' | 'full';
 export type Platform = 'darwin' | 'linux' | 'win32';
+
+// This is pretty gross. It's used on Windows to figure out how to set
+// up the PATH and will have to change every time the current snapshot
+// version changes.
+const SNAPSHOT_VERSION_SUFFIX = '7.5.0.6';
 
 const RACKET_ARCHS: {[key: string]: string} = {
   'x86-darwin': 'i386',
@@ -70,6 +76,68 @@ export function makeInstallerURL(
   }
 }
 
+async function installLinux(path: string) {
+  await fs.promises.writeFile(
+    '/tmp/install-racket.sh',
+    `
+echo "yes\n1\n" | sudo sh ${path} --create-dir --unix-style --dest /usr/
+`
+  );
+  await exec.exec('sh', ['/tmp/install-racket.sh']);
+}
+
+async function installWin32(version: string, arch: Arch, path: string) {
+  await fs.promises.mkdir('chocopkg');
+  await fs.promises.mkdir('chocopkg/tools');
+  process.chdir('chocopkg');
+
+  await fs.promises.writeFile(
+    'racket.nuspec',
+    `<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd">
+  <metadata>
+    <id>racket</id>
+    <version>${version === 'current' ? '7.999' : version}</version>
+    <title>Racket</title>
+    <authors>PLT Inc.</authors>
+    <description>The Racket programming language.</description>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+  </metadata>
+</package>
+`
+  );
+
+  await fs.promises.writeFile(
+    'tools/chocolateyInstall.ps1',
+    `
+$toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+        packageName = 'racket'
+        silentArgs = '/S'
+        fileType = 'exe'
+        ${arch === 'x86' ? 'file' : 'file64'} = '${path}'
+        validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+`
+  );
+
+  await exec.exec('choco pack');
+  await exec.exec('choco install racket -s .');
+
+  const installSuffix =
+    version === 'current' ? `-${SNAPSHOT_VERSION_SUFFIX}` : '';
+  let installPath: string;
+  if (arch === 'x86') {
+    installPath = `C:\\Program Files (x86)\\Racket${installSuffix}`;
+  } else {
+    installPath = `C:\\Program Files\\Racket${installSuffix}`;
+  }
+
+  core.addPath(installPath);
+}
+
 export async function install(
   version: string,
   arch: Arch,
@@ -86,13 +154,14 @@ export async function install(
     )
   );
 
-  await fs.promises.writeFile(
-    '/tmp/install-racket.sh',
-    `
-echo "yes\n1\n" | sudo sh ${path} --create-dir --unix-style --dest /usr/
-`
-  );
-  await exec.exec('sh', ['/tmp/install-racket.sh']);
+  switch (process.platform as Platform) {
+    case 'linux':
+      return await installLinux(path);
+    case 'win32':
+      return await installWin32(version, arch, path);
+    default:
+      throw new Error(`platform '${process.platform}' is not yet supported`);
+  }
 }
 
 export function parseArch(s: string): Arch {
